730a07dab4aed07217d8494265b637e8cc9a38c6
Sinaí <sanchez.yesleny@gmail.com>
2025-02-04 21:17:47 -0600

    JS-desde-cero

---
+++ web-e-js-desde-cero/1-fundamentos-js/tipos-de-dato.js
@@ -0,0 +1,19 @@
+//Verificando el tipo de dato
+//Ejercicios propuestos
+console.log(typeof "redonda");
+console.log(typeof 42);
+console.log(typeof "Veinticinco");
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof "");
+console.log(typeof null);
+console.log(typeof 'redonda');
+console.log(typeof undefined);
+console.log(typeof FALSE);
+//Más ejercicios de práctica
+console.log(typeof 25 + 25);
+console.log(typeof 3.6);
+console.log(typeof 3.6 + "3.6" + 3.6);
+console.log(typeof "string");
+console.log(typeof "string" + "string");
