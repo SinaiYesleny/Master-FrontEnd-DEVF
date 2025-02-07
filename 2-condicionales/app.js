6dc6047d302451e37a12ddddd3d298936e508895
Sinaí <sanchez.yesleny@gmail.com>
2025-02-06 20:58:02 -0600

    modulo2-comparaciones y decisiones en js

---
+++ 2-condicionales/app.js
@@ -0,0 +1,28 @@
+/*var
+var color = "Amarillo";
+var color = "Azul";
+
+console.log(color);*/
+
+/*let
+let nombre = "carlos";
+nombre = "Pedro"; //esto no funciona
+
+let nombre = "Luis";
+console.log(nombre); //Vemos que se deja reasignar la variable pero no reedeclararla
+*/
+/*//const
+const color = 'blanco';
+color = 'verde';
+
+console.log(color); //no me deja reasignar porque ya estaba declarada
+*/
+
+/*
+//operadores
+//operadores matemáticos
+let shots = 3;
+console.log(shots + 3);
+*/
+10%3; //devuelve el residuo que es 
+//el += es un operador de asignación combinado con la suma
---
+++ 2-condicionales/práctica/app.js
@@ -0,0 +1,18 @@
+//Práctica: Evaluador de Notas con Mensajes Personalizados 
+
+let notaEstudiante = Number(prompt('Introduzca su nota'));
+/* Opcional - Para generar notas aleatorias
+let notaEstudiante = Math.random() * 100*/
+
+if(notaEstudiante > 0 && notaEstudiante <=100){
+    if (notaEstudiante >=90){
+        console.log('Excelente');
+    } else if (notaEstudiante <=89 && notaEstudiante >=75){
+        console.log('Bien');    
+    } else if (notaEstudiante <=74 && notaEstudiante >=60){
+        console.log('Suficiente');
+    } else {
+        console.log('No aprobado');}
+} else {
+    console.log('Introduzca una nota válida');
+}
